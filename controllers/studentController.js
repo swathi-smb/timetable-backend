41a8b046f1e8778be504ce11d5d33809da8c3c41
unknown <sswathismmb@gmail.com>
2025-06-13 02:19:36 +0530

    updated delete cascade of student

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -1,4 +1,4 @@
-import { pool } from '../config/db.js';
+import pool  from '../config/db.js';
 import Student from '../models/Student.js';
 import School from '../models/School.js';
 import Department from '../models/Department.js';
